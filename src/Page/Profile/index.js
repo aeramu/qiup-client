53afac4fced36acc4ffee67300d2bc647bbcab2c
aeramu <sulam3010@gmail.com>
2020-05-27 04:59:13 +0700

    [alam] search profile hook

--- src/Page/Profile/index.js
+++ src/Page/Profile/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default ({navigation}) => {
+  const {data} = navigation.state.params
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <TextInput/>
-      <TextInput/>
-      <Button title='login' onPress={()=>navigation.navigate('Home')}>Login</Button>
+      <Text>{data.account.username}</Text>
+      <Text>{data.account.profile.name}</Text>
+      <Text>{data.account.profile.bio}</Text>
     </View>
   );
 }
--- src/Page/Search/index.js
+++ src/Page/Search/index.js
@@ -1,11 +1,36 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {useLazyQuery} from '@apollo/react-hooks'
+import {gql} from 'apollo-boost'
+
+const ACCOUNT = gql`
+  query($id: ID!){
+    account(id: $id){
+      username
+      profile{
+        name
+        bio
+      }
+    }
+  }
+`
 
 export default ({navigation}) => {
-  const [username, setUsername] = useState('')
+  const [id, setID] = useState('')
+  const [searchAccount,{data}] = useLazyQuery(
+    ACCOUNT, {
+      variables:{
+        id
+      },
+      onCompleted(data){
+        console.log(data)
+        navigation.navigate('Profile',{data})
+      }
+    }
+  )
 
   const handleSearch = () => {
-    navigation.navigate('Profile')
+    searchAccount()
   }
 
   return (
@@ -13,7 +38,8 @@ export default ({navigation}) => {
       <TextInput
         style={{ height: 40, borderWidth: 1, padding: 10, margin: 20 }}
         placeholder="Search..."
-        onChangeText={text => setUsername(text)}
+        autoCapitalize='none'
+        onChangeText={text => setID(text)}
       />
       <Button
         title="Search"
