0e72603c56a2eb30e5092b4aa45d57df93e6babc
aeramu <sulam3010@gmail.com>
2020-05-31 02:40:23 +0700

    [alam] bone design my profile

--- src/Page/EditProfile/index.js
+++ src/Page/EditProfile/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image, Dimensions, TouchableHighlight } from 'react-native';
 import {useMutation, useLazyQuery} from '@apollo/react-hooks'
 import {gql} from 'apollo-boost'
 import * as ImagePicker from 'expo-image-picker'
@@ -41,7 +41,7 @@ export default ({navigation}) => {
     const {base64} = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
-        aspect: [3, 4],
+        aspect: [3, 2],
         quality: 1,
         base64: true,
     })
@@ -106,50 +106,80 @@ export default ({navigation}) => {
       navigation.navigate('MyProfile')
     })
   }
-
   return (
-    <View style={styles.container}>
-      <Image
-        style={{width:200, height:200}}
-        source={{uri: coverPhoto}}
-      />
-      <Button
-        title='change cover photo'
-        onPress={handleChangeCoverPhoto}
-      />
-      <Image
-        style={{width:100, height:100}}
-        source={{uri: profilePhoto}}
-      />
-      <Button
-        title='change profile photo'
-        onPress={handleChangeProfilePhoto}
-      />
-      <TextInput
-        style={{ height: 40, borderWidth: 1, padding: 10, margin: 10 }}
-        value={name}
-        autoCapitalize="none"
-        onChangeText={text => setName(text)}
-      />
-      <TextInput
-        style={{ height: 40, borderWidth: 1, padding: 10, margin: 10 }}
-        value={bio}
-        autoCapitalize="none"
-        onChangeText={text => setBio(text)}
-      />
-      <Button
-        title='save'
-        onPress={handleEditProfile}
-      />
+    <View style={{flex:1}}>
+      <TouchableHighlight onPress={handleChangeCoverPhoto}>
+        <Image
+          style={{alignSelf:'stretch', height:Math.round(Dimensions.get('window').width*2/3)}}
+          source={{uri: coverPhoto}}
+        />
+      </TouchableHighlight>
+      <View style={{flex:1,paddingHorizontal:15}}>
+        <View style={{height:50,flexDirection:'row',alignItems:'flex-end'}}>
+          <TouchableHighlight
+            onPress={handleChangeProfilePhoto}
+            style={{width:100,height:100,borderRadius:100}}
+          >
+            <Image
+              style={{width:100, height:100, borderRadius:100, borderWidth:2, borderColor:'white'}}
+              source={{uri: data.me.profile.profilePhoto}}
+            />
+          </TouchableHighlight>
+        </View>
+        <TextInput
+          style={{height:40,borderWidth:1,padding:10,margin:10}}
+          value={name}
+          onChangeText={text => setName(text)}
+        />
+        <TextInput
+          style={{height:40,borderWidth:1,padding:10,margin:10}}
+          value={bio}
+          autoCapitalize="none"
+          onChangeText={text => setBio(text)}
+        />
+        <Button
+          title='save'
+          onPress={handleEditProfile}
+        />
+      </View>
     </View>
+    // <View style={{flex:1}}>
+    //   <TouchableHighlight
+    //     style={{height:Math.round(Dimensions.get('window').width*2/3)}}
+    //     onPress={handleChangeCoverPhoto}
+    //   >
+    //     <Image
+    //       style={{alignSelf:'stretch',height:Math.round(Dimensions.get('window').width*2/3)}}
+    //       source={{uri: coverPhoto}}
+    //     />
+    //   </TouchableHighlight>
+    //   <View style={{height:50,justifyContent:'flex-end',paddingHorizontal:15}}>
+    //     <TouchableHighlight
+    //       onPress={handleChangeProfilePhoto}
+    //       style={{width:100,height:100,borderRadius:100}}
+    //     >
+    //       <Image
+    //         style={{width:100,height:100,borderRadius:100,borderWidth:2,borderColor:'white'}}
+    //         source={{uri: profilePhoto}}
+    //       />
+    //     </TouchableHighlight>
+    //     <TextInput
+    //       style={{height:40,borderWidth:1,padding:10,margin:10}}
+    //       value={name}
+    //       autoCapitalize="none"
+    //       onChangeText={text => setName(text)}
+    //     />
+    //     <TextInput
+    //       style={{height:40,borderWidth:1,padding:10,margin:10}}
+    //       value={bio}
+    //       autoCapitalize="none"
+    //       onChangeText={text => setBio(text)}
+    //     />
+    //     <Button
+    //       title='save'
+    //       onPress={handleEditProfile}
+    //     />
+    //   </View>
+    // </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/Page/MyProfile/index.js
+++ src/Page/MyProfile/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image} from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ImageBackground, Dimensions} from 'react-native';
 import {useQuery} from '@apollo/react-hooks'
 import { gql } from 'apollo-boost';
 
@@ -22,37 +22,59 @@ export default ({navigation}) => {
   const {loading, data} = useQuery(MY_PROFILE)
 
   if (loading) return (
-    <View style={styles.container}>
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
       <Text>Loading....</Text>
     </View>
   )
 
   return (
-    <View style={styles.container}>
-      <Button
-        title='Edit Profile'
-        onPress={() => navigation.navigate('EditProfile',{data})}
-      />
+    <View style={{flex:1}}>
       <Image
-        style={{width: 200, height:200}}
+        style={{alignSelf:'stretch', height:Math.round(Dimensions.get('window').width*2/3)}}
         source={{uri: data.me.profile.coverPhoto}}
       />
-      <Image
-        style={{width:100, height: 100}}
-        source={{uri: data.me.profile.profilePhoto}}
-      />
-      <Text>@{data.me.username}</Text>
-      <Text>{data.me.profile.name}</Text>
-      <Text>{data.me.profile.bio}</Text>
+      <View style={{flex:1,paddingHorizontal:15}}>
+        <View style={{height:50,flexDirection:'row',alignItems:'flex-end'}}>
+          <Image
+            style={{width:100, height:100, borderRadius:100, borderWidth:2, borderColor:'white'}}
+            source={{uri: data.me.profile.profilePhoto}}
+          />
+          <View style={{flex:1,alignItems:'center'}}>
+            <Text style={{fontSize:20,fontWeight:'bold'}}>200</Text>
+            <Text>Posts</Text>
+          </View>
+          <View style={{flex:1,alignItems:'center'}}>
+            <Text style={{fontSize:20,fontWeight:'bold'}}>200</Text>
+            <Text>Followers</Text>
+          </View>
+          <View style={{flex:1,alignItems:'center'}}>
+            <Text style={{fontSize:20,fontWeight:'bold'}}>200</Text>
+            <Text>Following</Text>
+          </View>
+        </View>
+        <View style={{flexDirection:'row',marginTop:20}}>
+          <View style={{flex:3}}>
+            <Text style={{fontSize:20, fontWeight:'bold'}}>{data.me.profile.name}</Text>
+            <Text style={{color:'grey'}}>@{data.me.username}</Text>
+          </View>
+          <View style={{flex:2,justifyContent:'center'}}>
+            <Button
+              title='Edit Profile'
+              onPress={() => navigation.navigate('EditProfile',{data})}
+            />
+          </View>
+        </View>
+        <Text style={{marginTop:20,marginBottom:20}}>{data.me.profile.bio}</Text>
+      </View>
+      <View style={{marginTop:15,paddingHorizontal:15}}>
+        
+      </View>
+      <View style={{marginVertical:20,paddingHorizontal:15}}>
+        
+      </View>
+      <View style={{flexDirection:'row'}}>
+        
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
